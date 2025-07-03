324694792122ee167f20035599ea4dec52e08e31
John Ferlito <74108174+jjfcode@users.noreply.github.com>
2025-07-03 14:44:01 -0400

    fix a bug

--- Bills_tracker_v3/main_desktop.py
+++ Bills_tracker_v3/main_desktop.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
-from gui.main_window import MainWindow
+from src.gui.main_window import MainWindow
 
 if __name__ == "__main__":
     app = MainWindow()
